b371d8e2fdf75749306ce389be3ffef7423ce1c6
Dicksen Veloopillay <dicksen.veloopillay@proximitybbdo.fr>
2020-01-14 16:15:06 +0400

    Updates

---
+++ precache-manifest.764e687e2c9e88e9a2c1f1e2563ba787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a5ec835b0c5beef0ad2d3ec96d5661",
+    "url": "/react-burger-app/index.html"
+  },
+  {
+    "revision": "c984917e6b942d4597f5",
+    "url": "/react-burger-app/static/css/main.b7774d56.chunk.css"
+  },
+  {
+    "revision": "65efc246cf3e2c6d5dab",
+    "url": "/react-burger-app/static/js/2.5ae1aa28.chunk.js"
+  },
+  {
+    "revision": "c984917e6b942d4597f5",
+    "url": "/react-burger-app/static/js/main.62f6cbfb.chunk.js"
+  },
+  {
+    "revision": "0097d03ffb56d6e6d1d6",
+    "url": "/react-burger-app/static/js/runtime~main.1677f4ac.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-burger-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
